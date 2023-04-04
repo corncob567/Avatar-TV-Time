f0287e08c09f0df806f90ad4dbec9d8426004772
Bryan Cora <corabj@mail.uc.edu>
2023-04-04 17:00:44 -0400

    basic skeleton

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// GLOBAL VARIABLES
+let data;
+
+//-------------------------//
+
+d3.csv("/data/avatar_transcripts.csv")
+  .then(_data =>{
+
+
+    })
+.catch(error => {
+    console.log(error);
+});
+
